2870f7d405ead8307edb9d23284cf0c515d1eab8
Joshua Scoggins <Theoretically.x64@gmail.com>
2018-03-31 19:00:05 -0700

    Import the register convention over

--- Core.h
+++ Core.h
@@ -64,6 +64,37 @@ namespace cisc0 {
 	};
 	class Core {
 		public:
+			/**
+			 * Constants that are always relevant to cisc0
+			 */
+			enum ArchitectureConstants  {
+				RegisterCount = 16,
+				MaxInstructionCount = 16,
+				// we only have 16 registers to play with
+				R15 = RegisterCount - 1,
+				R14 = RegisterCount - 2,
+				R13 = RegisterCount - 3,
+				R12 = RegisterCount - 4,
+				R11 = RegisterCount - 5,
+				R10 = RegisterCount - 6,
+				R9  = RegisterCount - 7,
+				R8  = RegisterCount - 8,
+				R7  = RegisterCount - 9,
+				R6  = RegisterCount - 10,
+				R5  = RegisterCount - 11,
+				R4  = RegisterCount - 12,
+				R3  = RegisterCount - 13,
+				R2  = RegisterCount - 14,
+				R1  = RegisterCount - 15,
+				R0  = RegisterCount - 16,
+				InstructionPointer = R15,
+				StackPointer = R14,
+				CallStackPointer = R13, // second stack
+				/// used by load/store routines to describe the memory address
+				AddressRegister = R12,
+				/// used by load/store routines to describe the source or destination of the operation
+				ValueRegister = R11,
+			};
 			struct Extractable {
 				public:
 					Extractable() = default;
